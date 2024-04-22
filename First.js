3a98ebc8ea1abb93badaceb4eb21782a20542cec
Divesh Jain <76817491+imdiveshjain@users.noreply.github.com>
2024-04-22 12:42:05 +0530

    Add files via upload

---
+++ First.js
@@ -0,0 +1,59 @@
+//Traning ID = 13
+console.log("hello World");
+
+/*Data types
+Primitive            Non-Primitive              Functions
+  -objects                -number                 -tradtional functions   
+  -functions              -string                 -function expression
+
+var vs let vs const
+                     re-declaration              re-assignment
+var                     YES                            YES
+let                     NO                             YES
+const                   NO                             NO
+
+null: when the value is null or zero. 
+    e.g. let a = null;
+undeined: when the variable has a value but you don't know it. 
+    e.g.let b; 
+*/
+
+//Functions
+//    Tradtional Functions
+// function sum(a,b){
+//     return a+b;
+// }
+// let c = sum(4,4)
+// console.log(c);
+//      Function Expressions
+            //anonymous function: fucntions without a name 
+                // let add = function(a,b){
+                //     return a+b;
+                // }
+                // add(5,5);
+                // console.log(add);
+            // //arrow function
+            //     let sum = (a,b) =>  a+b;
+            //     c=sum(4,4);
+            //     console.log(c);
+
+//Arrays
+// let arr =[1,2,3,4,5];
+// {
+//     0: 2;
+//     1: 3;
+// }
+// console.log(arr[0]);
+
+
+
+
+//rest operator: accepts all the arguments which are entered
+function sum(...numbers){
+    let total = 0;
+    for(let num of numbers){
+        total += ṇnum;
+    }
+    return total;
+}
+console.log(sum(1,2,3,4,5,6));fhgfhfhgg
