7ea8bdb191ab010c396c9ff5727d170ce9e7ba7f
Divesh Jain <76817491+imdiveshjain@users.noreply.github.com>
2024-04-24 10:10:35 +0530

    Update learn.java

--- learn.java
+++ learn.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class learn {
 
     public static void main(String[] args) {
         System.out.print("Hello World");
+        System.out.println("Enter your age: ");
+        Scanner scn = new Scanner(System.in);
     }
 }
