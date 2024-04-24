2b0a7ab4442752662d49cb8b850f940f8c6f1c1d
Divesh Jain <76817491+imdiveshjain@users.noreply.github.com>
2024-04-24 13:03:16 +0530

    Update hashsett.java

--- hashsett.java
+++ hashsett.java
@@ -1,14 +1,15 @@
 import java.util.HashSet;
+import java.util.Set;
 
-public class hashmapp {
+public class hashsett{
     public static void main(String[] args) {
-        HashSet <Integer> hs = new HashSet<>();
-        set<Integer> hs2 = new HashSet<>();
+        HashSet<Integer> hs = new HashSet<>();
+        Set<Integer> hs2 = new HashSet<>();
 
         hs.add(2);
         hs.add(4);
         hs.add(6);
-        
+
         for (Integer elem : hs) {
             System.out.println(elem);
         }
