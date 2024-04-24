d907206c40bbb3de0146b7a0396bf0ea5d11459b
Divesh Jain <76817491+imdiveshjain@users.noreply.github.com>
2024-04-24 13:03:37 +0530

    Update hashmapp.java

--- hashmapp.java
+++ hashmapp.java
@@ -1,16 +1,11 @@
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class hashmapp {
     public static void main(String[] args) {
-        HashSet <Integer> hs = new HashSet<>();
-        set<Integer> hs2 = new HashSet<>();
-
-        hs.add(2);
-        hs.add(4);
-        hs.add(6);
-        
-        for (Integer elem : hs) {
-            System.out.println(elem);
-        }
+        HashMap<String, Integer> hm = new hashmapp();
+        hm.put("Divesh", 21);
+        hm.put("Jain", 21);
+        System.out.println(hm);
     }
 }
