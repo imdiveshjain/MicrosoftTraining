b278e7c22fb8aa243dffc0b3ed4367c3d36b7cc0
Divesh Jain <76817491+imdiveshjain@users.noreply.github.com>
2024-04-24 12:49:19 +0530

    Update ArrayListt.java

--- ArrayListt.java
+++ ArrayListt.java
@@ -15,5 +15,6 @@ public class ArrayListt {
         System.out.println(al.size());
         System.out.println(al.remove(0));
         System.out.println(al.contains(5));
+        System.out.println(al.indexOf(al));
     }
 }
