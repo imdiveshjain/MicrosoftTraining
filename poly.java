239fe2ea57bfaf7459b94bd8f4e477a3b1cc329b
Divesh Jain <76817491+imdiveshjain@users.noreply.github.com>
2024-04-25 10:44:11 +0530

    Update poly.java

---
+++ poly.java
@@ -0,0 +1,17 @@
+public class poly {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Calculator{
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int add(int a, int b, int c){
+        return a+b+c;
+    }
+
+    
+}
