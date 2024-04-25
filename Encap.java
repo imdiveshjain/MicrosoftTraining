e030f090239b5d86b02812583d50ba36ec9b0eb0
Divesh Jain <76817491+imdiveshjain@users.noreply.github.com>
2024-04-25 10:09:33 +0530

    Update Encap.java

--- Encap.java
+++ Encap.java
@@ -1,11 +1,16 @@
 public class Encap {
     public static void main(String[] args) {
         
+        Student s1 = new Student();
+
     }
 }
 
 class Student{
-    int id;
-    String name;
-    int rollNo;
+    private int id;
+    private String name;
+    private int rollNo;
+    void display(){
+        System.out.println(id + name + rollNo);
+    }
 }
