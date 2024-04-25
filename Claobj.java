459f044db629cfc49142758b66824bca67ba21dc
Divesh Jain <76817491+imdiveshjain@users.noreply.github.com>
2024-04-25 09:47:40 +0530

    Update Claobj.java

--- Claobj.java
+++ Claobj.java
@@ -2,6 +2,11 @@ public class Claobj {
     public static void main(String[] args) {
     
         Car c1 = new Car();
+        c1.brand = "Toyota";
+        c1.year = 2024;
+
+        System.out.println(c1.brand);
+        System.out.println(c1.year);
     }
 }
 class Car{
