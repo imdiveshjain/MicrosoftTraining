be7099ae3f2fcd40ae5e8460f9df2d18f36e8651
Divesh Jain <76817491+imdiveshjain@users.noreply.github.com>
2024-04-25 10:22:56 +0530

    Update Encap.java

--- Encap.java
+++ Encap.java
@@ -1,14 +1,17 @@
 public class Encap {
     public static void main(String[] args) {
-        
         Student s1 = new Student();
         s1.setId(1);
         s1.setName("DJ");
         s1.setRollNo(89);
+        s1.setAge(21);
 
         System.out.println(s1.getId());
         System.out.println(s1.getName());
         System.out.println(s1.getRollNo());
+        if(s1.getAge() > 18) {
+            System.out.println(s1.getAge());
+        }
     }
 }
 
@@ -16,11 +19,12 @@ class Student{
     private int id;
     private String name;
     private int rollNo;
+    private int age;
     
     public int getId(){
         return this.id;
     }
-    
+
     public void setId(int id){
         this.id = id;
     }
@@ -39,6 +43,14 @@ class Student{
     
     public void setRollNo(int rollNo){
         this.rollNo = rollNo;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+        
+    public void setAge(int age){
+        this.age = age;
     }
 
     void display(){
